c881324dd5a6e2d5cce2e16dcc8a51fa85400b75
Renzo Romeo <renzoromeo44@gmail.com>
2022-01-30 20:39:23 -0300

    dark mode default

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'dark'
+};
+
+const customTheme = extendTheme(config);
+
 function MyApp({ Component, pageProps }) {
   library.add(fab, faEnvelope);
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Head>
         <title>Renzo Romeo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
